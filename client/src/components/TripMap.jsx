5f792d8a86768b5610a82d179d4c57ef11b78b59
Lawrence Wang <lwang801118@gmail.com>
2023-08-09 21:55:57 +0000

    revised map component

--- client/src/components/TripMap.jsx
+++ client/src/components/TripMap.jsx
@@ -2,18 +2,14 @@ import { useMemo } from 'react';
 import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
 
 export default function TripMap() {
+  const event1 = useMemo(() => ({ lat: 37.3279997, lng: -119.6493154 }), []);
+  const event2 = useMemo(() => ({ lat: 37.7384615, lng: -119.5748224 }), []);
+
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: 'AIzaSyAJmRKjp79AFfguS6Pp50Zy3LSK6N9P0zs',
   });
 
   if (!isLoaded) return <div>Loading...</div>;
-  return <Map />;
-}
-
-function Map() {
-  const event1 = useMemo(() => ({ lat: 37.3279997, lng: -119.6493154 }), []);
-  const event2 = useMemo(() => ({ lat: 37.7384615, lng: -119.5748224 }), []);
-
   return (
     <GoogleMap zoom={10} center={event1} mapContainerClassName="map-container">
       <MarkerF position={event1} />
