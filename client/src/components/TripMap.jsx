ec8d89e90eb4f670450d80d03d590cbf79d32b71
Lawrence Wang <lwang801118@gmail.com>
2023-08-09 19:42:22 +0000

    created shell google map component

---
+++ client/src/components/TripMap.jsx
@@ -0,0 +1,28 @@
+import { useMemo } from 'react';
+import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
+
+export default function TripMap() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyAJmRKjp79AFfguS6Pp50Zy3LSK6N9P0zs',
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
+}
+
+function Map() {
+  const event1 = useMemo(() => ({ lat: 37.3279997, lng: -119.6493154 }), []);
+  const event2 = useMemo(() => ({ lat: 37.7384615, lng: -119.5748224 }), []);
+
+  return (
+    <GoogleMap zoom={10} center={event1} mapContainerClassName="map-container">
+      <MarkerF position={event1} />
+      <MarkerF
+        position={event2}
+        icon={{
+          url: 'https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=1|FF0000|000000',
+        }}
+      />
+    </GoogleMap>
+  );
+}
