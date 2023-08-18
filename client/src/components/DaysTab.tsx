c92f2bf9120d89b4143efeaa0e43af5530f37b46
Lawrence Wang <lwang801118@gmail.com>
2023-08-18 07:23:34 +0000

    save the changes in DaysTab component where showMapDay is changed to toggleMapDay

--- client/src/components/DaysTab.tsx
+++ client/src/components/DaysTab.tsx
@@ -1,20 +1,20 @@
 type DaysTabProps = {
   activeMapDays: number[];
   daysCount: number;
-  showMapDay: (i: number) => void;
+  toggleMapDay: (i: number) => void;
 };
 
 // creates a series of buttons that'll toggle the map markers by day
 export default function DaysTab({
   activeMapDays,
   daysCount,
-  showMapDay,
+  toggleMapDay,
 }: DaysTabProps) {
   const daysTabs = [];
   for (let i = 0; i < daysCount; i++) {
     daysTabs.push(
       <div
-        onClick={() => showMapDay(i)}
+        onClick={() => toggleMapDay(i)}
         className={`${
           activeMapDays.includes(i) ? 'bg-[#025464] text-white' : 'bg-gray-300'
         } flex justify-center items-center ml-2 w-8 h-8 rounded-full text-center cursor-pointer shadow hover:text-lg hover:font-semibold active:text-white active:bg-[#025464]`}>
